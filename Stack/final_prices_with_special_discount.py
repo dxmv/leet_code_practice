c9551de2766fa7f9e2693b50dfe29617f26f191b
dxmv <dimitirjestepanovic1@gmail.com>
2023-09-18 10:06:06 +0200

    1475. Final Prices With a Special Discount in a Shop Explanation

--- Stack/final_prices_with_special_discount.py
+++ Stack/final_prices_with_special_discount.py
@@ -1,5 +1,11 @@
 # 1475. Final Prices With a Special Discount in a Shop
 
+# Explanation:    
+# The solution is based around using the monotonic stack
+# The stack is always in the ascending order
+# We just subtract the next smallest number in stack from the current number, which is always the top of the stack, because we pop all the numbers larger than the current number in the stack
+# If there is no next smallest number than we just add the current number to the stack
+
 class Solution:
     # Time: O(n), Memory: O(n)
     def finalPrices(self, prices: List[int]) -> List[int]:
